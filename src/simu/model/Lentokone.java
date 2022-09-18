5dfa22142fe3920ba5f9fd0f6e1322740dfbd770
Moikonna <moikamut@gmail.com>
2022-09-18 22:17:25 +0300

    Update Lentokone.java

--- src/simu/model/Lentokone.java
+++ src/simu/model/Lentokone.java
@@ -2,18 +2,18 @@ package simu.model;
 
 public class Lentokone {
 	private int id;
-	private int totalplanes = 0;
+	private int totalPlanes = 0;
 	private FlightType flightType;
 	private int passengerCount;
 
 	public Lentokone() {
-		totalplanes++;
-		id = totalplanes;
+		totalPlanes++;
+		id = totalPlanes;
 	}
 
 	public Lentokone(FlightType flightType, int passengerCount) {
-		totalplanes++;
-		id = totalplanes;
+		totalPlanes++;
+		id = totalPlanes;
 		this.flightType = flightType;
 		this.passengerCount = passengerCount;
 	}
