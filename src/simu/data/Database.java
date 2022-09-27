f3dd5984b6a70d055c129ea6c56b64b624872291
Henri Vuento <36409785+moikonna@users.noreply.github.com>
2022-09-27 10:54:36 +0300

    Update Database.java

--- src/simu/data/Database.java
+++ src/simu/data/Database.java
@@ -52,18 +52,48 @@ public class Database {
 		}
 
 	}
+	public void writeToDatabase() throws Exception {
+		try {
+			// This will load the MySQL driver, each DB has its own driver
+			Class.forName("com.mysql.jdbc.Driver");
+			// Setup the connection with the DB
+			connect = DriverManager.getConnection("jdbc:mysql://localhost/test?" + "user=root&password=root");
 
-	private void writeMetaData(ResultSet resultSet) throws SQLException {
-		// Now get some metadata from the database
-		// Result set get the result of the SQL query
+			preparedStatement = connect.prepareStatement("insert into  test values (default, ?)");
+			// "insert test data to test database");
+			
+			// Parameters start with 1
+			preparedStatement.setString(1, "yeet");
+			preparedStatement.executeUpdate();
 
-		System.out.println("The columns in the table are: ");
-
-		System.out.println("Table: " + resultSet.getMetaData().getTableName(1));
-		for (int i = 1; i <= resultSet.getMetaData().getColumnCount(); i++) {
-			System.out.println("Column " + i + " " + resultSet.getMetaData().getColumnName(i));
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			close();
 		}
+
 	}
+	public void getAllFromDatabase() throws Exception {
+		try {
+			// This will load the MySQL driver, each DB has its own driver
+			Class.forName("com.mysql.jdbc.Driver");
+			// Setup the connection with the DB
+			connect = DriverManager.getConnection("jdbc:mysql://localhost/test?" + "user=root&password=root");
+
+			// Statements allow to issue SQL queries to the database
+			statement = connect.createStatement();
+			// Result set get the result of the SQL query
+			resultSet = statement.executeQuery("select * from test");
+			writeResultSet(resultSet);
+
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			close();
+		}
+
+	}
+
 
 	private void writeResultSet(ResultSet resultSet) throws SQLException {
 		// ResultSet is initially before the first data set
