c0aa137785ffcb77f3eba1a2e3642eaf8b6eb808
Moikonna <moikamut@gmail.com>
2022-10-09 17:57:05 +0300

    Create testTable.sql

---
+++ Sql/testTable.sql
@@ -0,0 +1,15 @@
+CREATE TABLE test (
+    ID int NOT NULL AUTO_INCREMENT,
+	checkinA double,
+    checkinMed double,
+    baggageA double,
+    baggageMed double,
+    securityA double,
+	securityMed double,
+    PassportA double,
+    passportMed double,
+    TicketA double,
+    TicketMed double,
+    AsiakasAv double,
+    PRIMARY KEY (ID)
+);
